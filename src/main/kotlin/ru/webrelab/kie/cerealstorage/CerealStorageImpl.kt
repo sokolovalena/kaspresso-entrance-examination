7f7a4a8734bc7918cfbb829e0cc6e8938264796d
sokolova_el <sokolova_el@taximaxim.ru>
2026-01-12 18:16:16 +0300

    kaspresso-exam

--- src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
+++ src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
@@ -20,4 +20,84 @@ class CerealStorageImpl(
 
     private val storage = mutableMapOf<Cereal, Float>()
 
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        if (amount < 0) {
+            throw IllegalArgumentException("Передано отрицательное значение")
+        }
+
+        // Если такой крупы еще нет, попытаемся добавить контейнер
+        if (!storage.containsKey(cereal)) {
+            if ((storage.size + 1) * containerCapacity > storageCapacity) {
+                throw IllegalStateException("В хранилище нет места для еще одного контейнера")
+            }
+            storage[cereal] = 0f
+        }
+
+        val current = storage[cereal] ?: 0f
+        val space = containerCapacity - current
+
+        if (amount > space) {
+            storage[cereal] = containerCapacity
+            return amount - space
+        } else {
+            storage[cereal] = current + amount
+            return 0f
+        }
+    }
+
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        if (amount < 0f) {
+            throw IllegalArgumentException("Передано отрицательное значение")
+        }
+
+        val current = storage[cereal] ?: 0f
+
+        if (amount > current) {
+            storage[cereal] = 0f
+            return current
+        } else {
+            return amount
+        }
+    }
+
+    override fun removeContainer(cereal: Cereal): Boolean {
+        if (!storage.containsKey(cereal)) {
+            return false
+        }
+
+        val current = storage[cereal] ?: 0f
+
+        if (current == 0.0f) {
+            storage.remove(cereal)
+            return true
+        }
+
+        return false
+    }
+
+    override fun getAmount(cereal: Cereal): Float {
+        if (!storage.containsKey(cereal)) {
+            return 0f
+        }
+        return storage[cereal] ?: 0f
+    }
+
+    override fun getSpace(cereal: Cereal): Float {
+        if (!storage.containsKey(cereal)) {
+            throw IllegalStateException("Контейнера этой крупы нет")
+        }
+
+        val current = storage[cereal] ?: 0f
+        val space = containerCapacity - current
+        return space
+    }
+
+    override fun toString(): String {
+        var res = "Хранилище:"
+        for (entry in storage) {
+            res += "\n${entry.key}: ${entry.value}"
+        }
+        return res
+    }
+
 }
--- src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
+++ src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
@@ -13,4 +13,99 @@ class CerealStorageImplTest {
             CerealStorageImpl(-4f, 10f)
         }
     }
+
+    @Test
+    fun `should throw if storageCapacity is negative`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            CerealStorageImpl(4f, -10f)
+        }
+    }
+
+    @Test
+    fun `should throw if storageCapacity is less than containerCapacity`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            CerealStorageImpl(10f, 5f)
+        }
+    }
+
+    @Test
+    fun `addCereal should throw if amount is negative`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            storage.addCereal(Cereal.RICE, -1f)
+        }
+    }
+
+    @Test
+    fun `addCereal should throw if no space for new container`() {
+        storage.addCereal(Cereal.PEAS, 10f)
+        storage.addCereal(Cereal.BUCKWHEAT, 10f)
+        assertThrows(IllegalStateException::class.java) {
+            storage.addCereal(Cereal.MILLET, 1f)
+        }
+    }
+
+    @Test
+    fun `addCereal should return remainder if container full`() {
+        val remainder = storage.addCereal(Cereal.BULGUR, 17f)
+        assertEquals(7f, remainder, 0.01f)
+    }
+
+    @Test
+    fun `getCereal should throw if amount negative`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            storage.getCereal(Cereal.MILLET, -6f)
+        }
+    }
+
+    @Test
+    fun `getCereal should return available amount if requested more`() {
+        storage.addCereal(Cereal.BULGUR, 5f)
+        val take = storage.getCereal(Cereal.BULGUR, 10f)
+        assertEquals(5f, take, 0.01f)
+        assertEquals(0f, storage.getAmount(Cereal.BULGUR), 0.01f)
+    }
+
+    @Test
+    fun `removeContainer should return true if container is empty`() {
+        storage.addCereal(Cereal.BUCKWHEAT, 0f)
+        assertTrue(storage.removeContainer(Cereal.BUCKWHEAT))
+    }
+
+    @Test
+    fun `removeContainer should return false if container is not empty`() {
+        storage.addCereal(Cereal.RICE, 5f)
+        assertFalse(storage.removeContainer(Cereal.RICE))
+    }
+
+    @Test
+    fun `removeContainer after adding and then taking all cereal`() {
+        storage.addCereal(Cereal.RICE, 5f)
+        val take = storage.getCereal(Cereal.RICE, 6f)
+        assertTrue(storage.removeContainer(Cereal.RICE))
+    }
+
+    @Test
+    fun `getAmount should return 0 if no container`() {
+        assertEquals(0f, storage.getAmount(Cereal.MILLET), 0.01f)
+    }
+
+    @Test
+    fun `getSpace should return space`() {
+        storage.addCereal(Cereal.PEAS, 7f)
+        assertEquals(3f, storage.getSpace(Cereal.PEAS), 0.01f)
+    }
+
+    @Test
+    fun `getSpace should throw if not container`() {
+        assertThrows(IllegalStateException::class.java) {
+            storage.getSpace(Cereal.BULGUR)
+        }
+    }
+
+    @Test
+    fun `toString should return no empty string`() {
+        storage.addCereal(Cereal.BUCKWHEAT, 5f)
+        assertTrue(storage.toString().contains("BUCKWHEAT"))
+    }
+
 }
